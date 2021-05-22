9fb88f51d8ae9934d1d6613f7bf6a169de316849
SantiGO055 <santigonzalez05@gmail.com>
2021-05-22 20:16:21 -0300

    spinner terminado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
@@ -14,7 +13,7 @@ import { SpinnerComponent } from './spinner/spinner.component';
     IonicModule,
     
   ],
-  declarations: [SpinnerComponent],
-  exports:[SpinnerComponent]
+  declarations: [],
+  exports:[]
 })
 export class ComponentsPageModule {}
