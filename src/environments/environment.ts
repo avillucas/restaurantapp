433eaa445d871d35761daad55a9ef8bac9168d73
SantiGO055 <santigonzalez05@gmail.com>
2021-05-22 13:02:35 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyCul8xhvFn6Td20UCtrx7gmjGPVPSYKQeU",
+    authDomain: "restauranteapp-bffc6.firebaseapp.com",
+    projectId: "restauranteapp-bffc6",
+    storageBucket: "restauranteapp-bffc6.appspot.com",
+    messagingSenderId: "237447339541",
+    appId: "1:237447339541:web:9927e7b8e8785a1c98781c",
+    measurementId: "G-SJ5WL6P57J"
 };
 
 /*
