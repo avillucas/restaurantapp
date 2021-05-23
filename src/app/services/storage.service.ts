a496f54912d514908b9842e76f9b4ec4d5c6e264
Lucas Avila <lucas_avila@div-it.com.ar>
2021-05-22 22:31:07 -0300

    cambios

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,58 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Plugins } from "@capacitor/core";
+import { Plugins } from '@capacitor/core';
 import { LoginResponse } from '../entities/loginResponse';
 const { Storage } = Plugins;
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
-
   readonly AUTH_KEY = 'token';
   readonly ROL_KEY = 'rol';
   readonly USER_ID_KEY = 'userId';
-  
-  constructor() {
-  }
-    
+
+  constructor() {}
+
   async set(key: string, value: any): Promise<void> {
     await Storage.set({
       key: key,
-      value: JSON.stringify(value)
+      value: JSON.stringify(value),
     });
   }
 
-  async setLoginData(response:LoginResponse): Promise<void> {
+  async setLoginData(response: LoginResponse): Promise<void> {
     await Storage.set({
       key: this.AUTH_KEY,
-      value: JSON.stringify(response.token)
-    });        
+      value: JSON.stringify(response.token),
+    });
     await Storage.set({
       key: this.ROL_KEY,
-      value: JSON.stringify(response.rol)
+      value: JSON.stringify(response.rol),
     });
     await Storage.set({
       key: this.USER_ID_KEY,
-      value: JSON.stringify(response.userId)
+      value: JSON.stringify(response.userId),
     });
   }
-  
+
   async get(key: string): Promise<any> {
     const item = await Storage.get({ key: key });
     return JSON.parse(item.value);
   }
-  
+
   async remove(key: string): Promise<void> {
     await Storage.remove({
-      key: key
+      key: key,
     });
   }
 
-  logout(){
+  logout() {
     this.remove(this.AUTH_KEY);
     this.remove(this.ROL_KEY);
     this.remove(this.USER_ID_KEY);
   }
-
 }
